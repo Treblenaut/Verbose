ea815cf303925b8d186e2d9e7e07c1321780d325
Treblenaut <38347808+Treblenaut@users.noreply.github.com>
2022-08-09 18:47:13 +0200

    Added color palette.

--- app/src/main/java/com/baslundvisuals/verbose/ui/theme/Color.kt
+++ app/src/main/java/com/baslundvisuals/verbose/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package com.baslundvisuals.verbose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Yellow400 = Color(0xFFFDB851)
+val Red400 = Color(0xFFFE7F45)
+val Neutral100 = Color(0xFFFFFFFF)
+val Neutral200 = Color(0xFFF5F5F4)
+val Neutral300 = Color(0xFFECEBEA)
+val Neutral400 = Color(0xFFCDCCCA)
+val Neutral500 = Color(0xFF9C9A97)
+val Neutral700 = Color(0xFF6A6762)
+val Neutral900 = Color(0xFF38342D)
